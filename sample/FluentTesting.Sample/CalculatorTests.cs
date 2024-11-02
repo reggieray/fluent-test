a9088133a76ac98aa39bddcb8ceca7e553b5f19e
regis <regis12@gmail.com>
2024-11-02 23:05:35 +0000

    init

---
+++ sample/FluentTesting.Sample/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace FluentTesting.Sample;
+
+public class CalculatorTests
+{
+    private readonly CalculatorTestSteps _calculatorTestSteps = new();
+    
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(4, 5, 9)]
+    [InlineData(22, 33, 55)]
+    public void ShouldAdd(int a, int b, int expected)
+    {
+        _calculatorTestSteps
+            .Given("A calculator")
+            .When(x => x.TwoNumbersAreAdded(a, b))
+            .Then(x => x.TheNumberShouldEqual(expected))
+            .Test();
+    }
+}
+
+public class CalculatorTestSteps
+{
+    private readonly Calculator.Calculator _calculator = new();
+    private double _addResult;
+
+    public void TwoNumbersAreAdded(int a, int b)
+    {
+        _addResult = _calculator.Add(a, b);
+    }
+
+    public void TheNumberShouldEqual(int expected)
+    {
+        _addResult.Should().Be(expected);
+    }
+}
---
+++ src/FluentTesting/FluentTest.cs
@@ -0,0 +1,99 @@
+﻿namespace FluentTesting;
+
+public class FluentTest<T>(T subject)
+{
+    private readonly List<Action> _givenActions = [];
+    private readonly List<Action> _whenActions = [];
+    private readonly List<Action> _thenActions = [];
+
+    public FluentTest<T> Given(Action<T> setup)
+    {
+        _givenActions.Add(() => setup(subject));
+        return this;
+    }
+
+    public FluentTest<T> When(Action<T> action)
+    {
+        _whenActions.Add(() => action(subject));
+        return this;
+    }
+
+    public FluentTest<T> Then(Action<T> assertion)
+    {
+        _thenActions.Add(() => assertion(subject));
+        return this;
+    }
+
+    public FluentTest<T> And(Action<T> additionalAction)
+    {
+        if (_thenActions.Count > 0)
+        {
+            _thenActions.Add(() => additionalAction(subject));
+            return this;
+        }
+        
+        if (_whenActions.Count > 0)
+        {
+            _whenActions.Add(() => additionalAction(subject));
+            return this;
+        }
+        
+        _givenActions.Add(() => additionalAction(subject));
+        return this;
+    }
+
+    public void Test()
+    {
+        foreach (var action in _givenActions) action();
+        foreach (var action in _whenActions) action();
+        foreach (var action in _thenActions) action();
+    }
+}
+
+public static class FluentTestExtensions
+{
+    public static FluentTest<T> Given<T>(this T subject, string message)
+    {
+        return new FluentTest<T>(subject);
+    }
+    
+    public static FluentTest<T> Given<T>(this T subject, Action<T> setup)
+    {
+        return new FluentTest<T>(subject).Given(setup);
+    }
+
+    public static FluentTest<T> When<T>(this FluentTest<T> fluentTest, Action<T> action)
+    {
+        return fluentTest.When(action);
+    }
+    
+    public static FluentTest<T> When<T>(this FluentTest<T> fluentTest, string message)
+    {
+        return fluentTest;
+    }
+
+    public static FluentTest<T> Then<T>(this FluentTest<T> fluentTest, Action<T> assertion)
+    {
+        return fluentTest.Then(assertion);
+    }
+    
+    public static FluentTest<T> Then<T>(this FluentTest<T> fluentTest, string message)
+    {
+        return fluentTest;
+    }
+
+    public static FluentTest<T> And<T>(this FluentTest<T> fluentTest, Action<T> additionalAction)
+    {
+        return fluentTest.And(additionalAction);
+    }
+    
+    public static FluentTest<T> And<T>(this FluentTest<T> fluentTest, string message)
+    {
+        return fluentTest;
+    }
+
+    public static void Run<T>(this FluentTest<T> fluentTest)
+    {
+        fluentTest.Test();
+    }
+}
